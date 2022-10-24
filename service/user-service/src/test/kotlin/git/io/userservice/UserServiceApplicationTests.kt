c8ebc268b5faea0d23aba0df61e7161d30b49870
changhee10 <leech8643@hanpass.com>
2022-10-24 14:27:07 +0900

    init

---
+++ service/user-service/src/test/kotlin/git/io/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
